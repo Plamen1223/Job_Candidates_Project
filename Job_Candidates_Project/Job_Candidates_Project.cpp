a32765cd8516f30224ea5adcaf544ccb5257a55f
GSLazarov18 <GSLazarov18@codingburgas.bg>
2021-07-09 10:17:04 +0300

    wokring on candidates array/changes in  the main menu

--- Job_Candidates_Project/Job_Candidates_Project.cpp
+++ Job_Candidates_Project/Job_Candidates_Project.cpp
@@ -11,6 +11,20 @@ struct CANDIDATE
 
 int main()
 {
-
+	CANDIDATE candidates[100];
+	int counter = 0;
+	cout << "Enter the number of candidates ";
+	cin >> counter;
+	for (size_t i = 0; i < counter; i++)
+	{	
+		candidates->id = i;
+		cout << "Enter a Name ";
+		cin >> candidates[i].name;
+		cout << "Enter age ";
+		cin >> candidates[i].age;
+		cout << "Enter a gender(male or female) ";
+		cin >> candidates[i].gender;
+	}
+	
 }
 
